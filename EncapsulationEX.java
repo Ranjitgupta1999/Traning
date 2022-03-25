c597b98b3858b397ffc69576e6bf161ad8c4010e
Ranjit <iskoza2001@gmail.com>
2022-03-25 09:22:32 +0530

    date 24/3/2022

---
+++ EncapsulationEX.java
@@ -0,0 +1,57 @@
+public class EncapsulationEX {
+    private String name;
+    private int age;
+    private int id;
+    private int BOD;
+    private Number Phonenumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Number getPhonenumber() {
+        return Phonenumber;
+    }
+
+    public void setPhonenumber(Number Phonenumber) {
+        this.Phonenumber = Phonenumber;
+    }
+
+    public int getBOD() {
+        return BOD;
+    }
+
+    public void setBOD(int BOD) {
+        this.BOD = BOD;
+    }
+
+    public static void main(String[] args) {
+        EncapsulationEX ob=new EncapsulationEX();
+        ob.setAge(19);
+        ob.setBOD(1999);
+        ob.setId(905);
+        ob.setPhonenumber(999999999);
+        ob.setName("Anuragh");
+        System.out.println("name-"+ob.getName()+"\n"+"age-"+ob.getAge()+"\n"+"BOD-"+ob.getBOD()+"\n"+"id-"+ob.getId()+"\n"+"phone-"+ob.getPhonenumber());
+    }
+}
